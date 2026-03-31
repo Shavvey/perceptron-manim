755297121642a087a2855e1eda31fbf7fef38db2
Cole Johnson <coledjohn@gmail.com>
2026-03-31 16:21:58 -0600

    Create simple default animation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
-def main():
-    pass
+import manim as mn
 
-if __name__ == "__main__":
-    main()
+
+class MakeCircle(mn.Scene):
+    def construct(self):
+        circle = mn.Circle()  # create a circle
+        circle.set_fill(mn.PINK, opacity=0.5)  # set color and transparency
+
+        square = mn.Square()  # create a square
+        square.flip(mn.RIGHT)  # flip horizontally
+        square.rotate(-3 * mn.TAU / 8)  # rotate a certain amount
+
+        self.play(mn.Create(square))  # animate the creation of the square
+        self.play(mn.Transform(square, circle))  # interpolate the square into the circle
+        self.play(mn.FadeOut(square))  # fade out animation
